d52ac3ec9b8fbc78be113e3880dd446c1cfafbbb
Steve Lipton <steve@makeapppie.com>
2018-12-27 09:50:39 -0600

    Initial Commit

---
+++ MultiselectionButton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiselectionButton
+//
+//  Created by Steven Lipton on 12/27/18.
+//  Copyright © 2018 Steven Lipton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
