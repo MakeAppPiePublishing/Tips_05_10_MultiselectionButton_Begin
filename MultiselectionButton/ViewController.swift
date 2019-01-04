ceda4ae0d56eb7a4cbffe3fd68d63a86cad1bfc0
Steve Lipton <steve@makeapppie.com>
2019-01-04 06:07:53 -0600

    Start state for Mutiselection button.

--- MultiselectionButton/ViewController.swift
+++ MultiselectionButton/ViewController.swift
@@ -2,8 +2,11 @@
 //  ViewController.swift
 //  MultiselectionButton
 //
-//  Created by Steven Lipton on 12/27/18.
-//  Copyright © 2018 Steven Lipton. All rights reserved.
+//
+//  An exercise file for iOS Development Tips Weekly
+//  by Steven Lipton (C)2018, All rights reserved
+//  For videos go to http://bit.ly/TipsLinkedInLearning
+//  For code go to http://bit.ly/AppPieGithub
 //
 
 import UIKit
