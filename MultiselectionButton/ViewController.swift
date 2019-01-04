e3b01b151581d80a3ac93ffa6bc138e6d03ac3b2
Steve Lipton <steve@makeapppie.com>
2019-01-04 05:59:54 -0600

    Initial Start state for Multiselection

--- MultiselectionButton/ViewController.swift
+++ MultiselectionButton/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var selectionLabel: UILabel!
+    
+    func allDeselected(){
+        
+    }
+    
+  
+    func selectDessert(button:UIButton){
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+    }
+    
+    
+    // Show a shadow as though the button is floating
+    func shadowOn(button:UIButton){
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.6
+        button.layer.shadowOffset = CGSize(width: 3, height: 3)
+        button.layer.shadowRadius = 6.0
+    }
+    // Hide the shadow behind the button as if button is close to surface.
+    func shadowOff(button:UIButton){
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.9
+        button.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+        button.layer.shadowRadius = 1.0
+    }
 
 }
 
